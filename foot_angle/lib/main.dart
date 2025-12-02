81223f72a19a794b53345bbf1cdb54e5775c3ef9
Pariterre <pariterre@hotmail.com>
2025-12-02 16:34:08 -0500

    Added a connexion to neurbiomech server

--- foot_angle/lib/main.dart
+++ foot_angle/lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:foot_angle/screens/config_page.dart';
 import 'package:foot_angle/screens/feedback_page.dart';
+import 'package:frontend_common/managers/neurobio_client.dart';
+import 'package:frontend_common/managers/predictions_manager.dart';
+import 'package:logging/logging.dart';
 
-void main() {
+Future<void> main() async {
+  // Configure logging
+  Logger.root.onRecord.listen((record) {
+    if (record.level >= Level.INFO) {
+      debugPrint('${record.level.name}: ${record.time}: ${record.message}');
+    }
+  });
+
+  const bool.fromEnvironment('USE_BACKEND_MOCK')
+      ? await NeurobioClientMock.instance.initialize()
+      : await NeurobioClient.instance.initialize();
+
+  await PredictionsManager.instance.initialize();
+
   runApp(const MyApp());
 }
 
@@ -13,13 +29,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vise ton pied',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
       initialRoute: ConfigPage.routeName,
       routes: {
-        FeedbackPage.routeName: (context) =>
-            const FeedbackPage(title: 'Vise ton pied'),
         ConfigPage.routeName: (context) =>
             const ConfigPage(title: 'Configuration'),
+        FeedbackPage.routeName: (context) =>
+            const FeedbackPage(title: 'Vise ton pied'),
       },
     );
   }
--- foot_angle/lib/screens/config_page.dart
+++ foot_angle/lib/screens/config_page.dart
@@ -1,23 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:foot_angle/screens/feedback_page.dart';
+import 'package:frontend_common/managers/neurobio_client.dart';
+import 'package:logging/logging.dart';
 
-class ConfigPage extends StatelessWidget {
+final _logger = Logger('ConfigPage');
+
+class ConfigPage extends StatefulWidget {
   static const String routeName = '/config';
 
   const ConfigPage({super.key, required this.title});
   final String title;
 
   @override
+  State<ConfigPage> createState() => _ConfigPageState();
+}
+
+class _ConfigPageState extends State<ConfigPage> {
+  Completer<void>? _isDoingSomething;
+  bool get _isBusy =>
+      _isDoingSomething != null && !_isDoingSomething!.isCompleted;
+
+  final neurobioClient = NeurobioClient.instance;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            ElevatedButton(
+              onPressed: _isBusy
+                  ? null
+                  : () async => await _doSomething(
+                      (neurobioClient.isConnected
+                          ? _disconnectServer
+                          : _connectServer),
+                    ),
+              child: Text(
+                neurobioClient.isConnected ? 'Déconnexion' : 'Connexion',
+              ),
+            ),
             const Text('Mettre la configuration ici'),
             ElevatedButton(
               onPressed: () {
@@ -31,5 +60,38 @@ class ConfigPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _connectServer() async {
+    final isSuccess = await neurobioClient.connect(nbOfRetries: 5);
+    if (!isSuccess) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('La connexion au serveur neurobiomech a échoué'),
+          ),
+        );
+      }
+    }
+  }
+
+  Future<void> _disconnectServer() async {
+    await neurobioClient.disconnect();
+  }
+
+  Future<void> _doSomething(Future<void> Function() action) async {
+    if (_isDoingSomething != null) await _isDoingSomething?.future;
+
+    _isDoingSomething = Completer<void>();
+    setState(() {});
+    try {
+      await action();
+    } catch (err, st) {
+      _logger.severe('Error during operation', err, st);
+    } finally {
+      _isDoingSomething?.complete();
+      _isDoingSomething = null;
+      setState(() {});
+    }
   }
 }
